3b9f03eaeece2ea7c1cfb7866d7ae3aed21e5a10
AbYahaya <yabdulrauf911@gmail.com>
2026-04-10 14:43:56 +0100

    collections

---
+++ Rust-practice/collections/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let transactions = vec![0, 100, 200, 300];
+    for i in transactions {
+        if i > 100 {
+            println!("Value {} is greater than 100", i);
+        }
+    }
+
+}
+
